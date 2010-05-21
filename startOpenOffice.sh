6ba9985e69a2674523bb65c981b33926a2223a90
Brian Kulyk <brian@kulyk.ca>
2010-05-21 21:04:30 +0000

    updated .startOpenOffice.sh to work on Ubuntu, eventually I'll need to make it work on any os.  Actually this could be apart of the pyMailMergeService

--- startOpenOffice.sh
+++ startOpenOffice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/lib/openoffice.org/program/soffice.bin -headless "-accept=socket,host=localhost,port=8100;urp"
+/usr/lib/openoffice/program/soffice -headless "-accept=socket,host=localhost,port=8100;urp" -nologo -nofirststartwizard
 
