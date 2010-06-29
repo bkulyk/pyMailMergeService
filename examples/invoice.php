9c272fea9dbd6bb0b9c57bd97b555e11b37b147a
Brian Kulyk <brian@kulyk.ca>
2010-06-29 16:15:19 +0000

    prop changes

--- examples/invoice.php
+++ examples/invoice.php
@@ -89,4 +89,50 @@ $doc = $pyMailMergeService->convert( 'invoice.odt', $params, $doctype );
 //write the file to the disk.
 //the doc comes back base64 encoded, because I was getting errors sending binary data via soap. Something about utf-8 chars only.
 file_put_contents( '/var/www/html/invoice.'.$doctype, base64_decode( $doc ) );
+
+class MailMergeService{
+	static protected $connection = null;
+	static protected $uri = "urn:approve";
+	static protected $host = "http://localhost:8888/";
+	protected function getConnecton() {
+		if( is_null( self::$connection ) )
+			return self::connect();
+		else
+			return self::$connection;
+	}
+	static protected function connect() {
+		self::$connection = new SoapClient( null, array( 'uri'=>self::$uri,'location'=>self::$host ) );
+		return self::$connection;
+	}
+	public function getTokens( $odtFilename ) {
+		return self::getConnection()->getTokens( $odtFilename );
+	}
+	public function convert( $odtFilename, $params, $doctype='pdf' ) {
+		return self::getConnection()->convert( $odtFilename, $params, $doctype );
+	}
+	public function pdf( $odtFilename, $params ) {
+		return self::getConnection()->pdf( $odtFilename, $params );
+	}
+	public function upload( $odtFilePath ) {
+		if( file_exists( $odtFilePath ) ) {
+			$noContent = false;
+			$zip = zip_open( $odtFilePath );
+			if( is_resource( $zip ) ) {
+				$notZip = false;
+				//make sure that 
+				do{
+	                $entry = zip_read( $zip );
+	            }while( $entry && zip_entry_name( $entry ) != "content.xml" );
+				if( zip_entry_name( $entry ) == "content.xml" ) {
+					//do upload
+				}else
+					$noContent = true;
+			}else
+				$notZip = true;
+			if( $notZip || $noContent ) 
+				throw new Exception( "File provided is not a valid ODT: $odtFilePath" );
+		}else
+			throw new Exception( "ODT file does not exist: $odtFilePath" );
+	}
+}
 ?>
