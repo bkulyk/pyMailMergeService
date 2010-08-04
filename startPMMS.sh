e6f9ed58f8d66778428d0f2c04fc7d549ef4a3f4
Brian Kulyk <brian@kulyk.ca>
2010-08-04 20:00:17 +0000

    i intended to comment the last commit better but accidentially hit enter so here it is ---- I added a start script for pymailmergeservice that restarts the serverice if it gets kacked, check the wiki installation page for usage.  Modified the startopenoffice script to work the same way.  Added a bunch of unit tests for the tokens regular experession, because they were much needed.

--- startPMMS.sh
+++ startPMMS.sh
@@ -3,6 +3,6 @@ sleep 2
 SCRIPT="/usr/bin/python /usr/share/pyMailMergeService/pyMailMergeService.py"
 while true; do
         cd /usr/share/pyMailMergeService
-        $SCRIPT  ######### The Service that runs
+        $SCRIPT  ######## The Service that runs
         sleep 1
 done
