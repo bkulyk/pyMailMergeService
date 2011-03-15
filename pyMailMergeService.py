9a3a9d0a9cddda2b92b6b26b8915258287c1f33e
Brian Kulyk <brian@kulyk.ca>
2011-03-14 22:28:15 -0500

    added method to convert uploaded document

--- pyMailMergeService.py
+++ pyMailMergeService.py
@@ -1,5 +1,6 @@
 import cherrypy as http
 import os.path
+import tempfile
 from pyMailMerge import pyMailMerge
 import simplejson as json
 class rest:
@@ -8,13 +9,26 @@ class rest:
     def index(self, test='default'):
         return """
         <html><body>
-        <form action='/test' method='post'>
         <h1>Hello World !</h1>
-        <input type='text' value='%s' name='var' />
-        <input type='submit' value='Submit' />
-        </form></body>
+        <p>%s</p>
+        </body>
         </html>
         """ % test
+    @http.expose
+    def uploadConvert( self, odt, params=[], type='pdf' ):
+        try:
+            file, fileName = tempfile.mkstemp( suffix="_pyMMS.odt" )
+            file.write( odt )
+            os.close( file )
+        except:
+            return self.__errorXML( '?', 'failed to write temporary odt file' )
+        try:
+            mms = pyMailMerge( fileName )
+            return mms.convert( params, type )
+        except:
+            number = "?"
+            message = "unknown exception"
+        return self.__errorXML( number, message )
     @http.expose
     def pdf( self, params='', odt='' ):
         return self.convert(params, odt, 'pdf')
@@ -22,23 +36,25 @@ class rest:
     def convert( self, params='', odt='', type='pdf' ):
         try:
             mms = pyMailMerge( self.documentBase + odt )
-            return mms.convert( params, 'pdf' )
+            return mms.convert( params, type )
         except:
             number = "?"
             message = "unknown exception"
         return self.__errorXML( number, message )
     @http.expose
-    def getTokens( self, odt='', format='json' ):
+    def getTokens( self, odt='', format='xml' ):
         try:
             mms = pyMailMerge( self.documentBase + odt )
             tokens = mms.getTokens()
             if format=='xml':
+                #convert params to xml
                 xml = """<?xml version="1.0" encoding="UTF-8"?><tokens>"""
                 for x in tokens:
                     xml += "<token>%s</token>" % x
                 xml += "</tokens>"
                 return xml
             else:
+                #return params as json encoded
                 return json.dumps( tokens )
         except:
             number = "?"
@@ -54,5 +70,4 @@ class rest:
             </error>
         </errors>""" % (number, message)
 if __name__ == '__main__':
-    http.root = rest()
-    http.server.start()
+    http.quickstart( rest() )
