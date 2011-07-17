72127e391b9dedd1d2143d04a7a9cafd268c90e2
Brian Kulyk <brian@kulyk.ca>
2011-07-17 15:09:32 -0500

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup( 
+       name='MailMergeService',
+       version='2.5', 
+       description='OpenOffice/LibreOffice mail merge like document creation implemented over webservice',
+       url="http://mailmergeservice.com",
+       packages=['mms', 'mms.interfaces', 'mms.modifiers', 'mms.OfficeDocument'],
+       scripts=['mmsd'],
+       install_requires=[
+            'pyPDF>=3',
+            'uno',
+            'cherrypy>=3',
+            'soappy>0.12' ] )
