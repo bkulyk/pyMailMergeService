38f9c4786c107c6cfe5f651108bb7db6f69c5db0
Brian Kulyk <brian@kulyk.ca>
2011-07-22 15:53:33 -0500

    added argparse requirement to setup file

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ from setuptools import setup
 setup( 
        name='MailMergeService',
        version='2.5', 
-       description='OpenOffice/LibreOffice mail merge like document creation implemented over webservice',
+       description='OpenOffice/LibreOffice mail merge like document creation implemented over a webservice',
        url="http://mailmergeservice.com",
        packages=['mms', 'mms.interfaces', 'mms.modifiers', 'mms.OfficeDocument'],
        scripts=['mmsd.py'],
        include_package_data=True,
        package_data={
-                "": [ "*" ] #all files in all modules
+                "": [ "*.cfg" ]
             },
        install_requires=[
 #                'pyPDF>=3.0',
 #                'uno',
                 'cherrypy>=3.0',
                 'soappy>0.12',
+                'argparse>1.1'
 #                'lxml>2.3' 
             ] )
